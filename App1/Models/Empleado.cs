daef1abead10e66c255d15c5efb4bcd2ccead70b
Dayana <Dayana@DESKTOP-BMET23Q>
2023-02-17 23:37:04 -0600

    Agregar archivos de proyecto.

---
+++ App1/Models/Empleado.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1.NewFolder
+{
+    public class Empleado
+    {
+        public int id { get; set; }
+
+        public string nombres { get; set; }
+
+        public string apeliidos { get; set;}
+
+        public string foto { get; set; }
+
+    }
+}
---
+++ App1/PagePrincipal.xaml.cs
@@ -0,0 +1,36 @@
+﻿using App1.NewFolder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PagePrincipal : ContentPage
+    {
+        public PagePrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btprocesar_Clicked(object sender, EventArgs e)
+        {
+            var emple = new Empleado
+            {
+                id = 1,
+                nombres = txtnombre.Text,
+                apeliidos = txtApellido.Text,
+                foto = null
+            };
+            //esto es para llamar una segunda pagina y pasar un parametro a una segunda pantalla//
+            var seconpage = new PageResultado();
+            seconpage.BindingContext = emple; //paso de parametros a la pantalla
+            Navigation.PushAsync(seconpage);
+        }
+    }
+}
